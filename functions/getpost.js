4364fa7f0b3317ba56f60cfd3de82b5e0243b957
Memehub <133832589+Memehub-Network@users.noreply.github.com>
2023-06-06 22:57:36 +0300

    Update getpost.js

--- functions/getpost.js
+++ functions/getpost.js
@@ -7,8 +7,12 @@ exports = async function(payload, response) {
   const bodyy = JSON.parse(payload.body.text());
   
   const pager = payload.query.page;
-  
-  var pageSize = 10;
+ 
+  var cluster = "mongodb-atlas";
+  var dBase = "memehubclient";
+  var coll = "posts";
+
+  var pageSize = 5;
   var pageNumber = 0;
   
   if(pager!=null){
@@ -17,20 +21,6 @@ exports = async function(payload, response) {
   }else{
    var pageQ = 0;
   }
-  
-  /*
-  if (bodyy.limit < 9) { 
-    var limit = 10
-  }else{
-    limit = bodyy.limit
-  }
-  */
-  //var limit = body.limit;
-  //var skip = bodyy.skip;
-  
-  var cluster = "mongodb-atlas";
-  var dBase = "memehubclient";
-  var coll = "posts";
   
    const collection = context.services.get(cluster).db(dBase).collection(coll);
    
@@ -56,10 +46,6 @@ exports = async function(payload, response) {
        return { error: err.message };
    }
    
-   return {
-     //page:pageQ,
-     results:results,
-     //total_results:pages
-   };
+   return { results:results };
   
 };
