fb026b5301eda6c8d8e51b775b91011af5010e6c
Memehub <133832589+Memehub-Network@users.noreply.github.com>
2023-06-07 18:28:22 +0300

    Update loginAuthentication.js

--- functions/loginAuthentication.js
+++ functions/loginAuthentication.js
@@ -9,7 +9,7 @@ var admin = require("firebase-admin");
 
 //var serviceAccount = require("path/to/serviceAccountKey.json");
 
-var serviceAccount = '/data/db/scripts/serviceAccountKey.json';
+var serviceAccount = 'scripts/serviceAccountKey.json';
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
