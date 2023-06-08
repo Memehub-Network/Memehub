ddb91e6c2e5c6ac6de21d6492fe6a4c38b5b66c3
Memehub <133832589+Memehub-Network@users.noreply.github.com>
2023-06-09 02:27:50 +0300

    Update Function_0.js

--- functions/Function_0.js
+++ functions/Function_0.js
@@ -1,56 +1,22 @@
-exports = async function(arg){
+exports = async function(email, password) {
+
+  var cluster = "mongodb-atlas"; 
+   var dBase = "authentication"; 
+   var coll = "users"; 
   
-  //const fb = require('firebase');
-  const { initializeApp } = require('firebase/app');
-const { getFirestore, collection, getDocs } = require('firebase/firestore');
-  
+    const usersCollection = context.services.get(cluster).db(dBase).collection(coll); 
+ 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAKBrTeGmSSyj6Rg37surQRshyfRn8t8dQ",
-  authDomain: "memehub-server.firebaseapp.com",
-  databaseURL: "https://memehub-server-default-rtdb.firebaseio.com",
-  projectId: "memehub-server",
-  storageBucket: "memehub-server.appspot.com",
-  messagingSenderId: "696200336899",
-  appId: "1:696200336899:web:6401e35efad652b677a036",
-  measurementId: "G-TGND5FYYL9"
-};
+  //const usersCollection = context.services.get("mongodb-atlas").db("your-database").collection("users");
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore(app);
+  // Find the user document based on the email
+  const user = await usersCollection.findOne({ email: email });
 
-// Get a list of cities from your database
-async function getCities(db) {
-  const citiesCol = collection($keys, 'cities');
-  const citySnapshot = await getDocs(citiesCol);
-  const cityList = citySnapshot.docs.map(doc => doc.data());
-  return cityList;
-}
-
-
-
-/*
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
-// Follow this pattern to import other Firebase services
-// import { } from 'firebase/<service>';
-
-// TODO: Replace the following with your app's Firebase project configuration
-const firebaseConfig = {
-  //...
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-// Get a list of cities from your database
-async function getCities(db) {
-  const citiesCol = collection(db, 'cities');
-  const citySnapshot = await getDocs(citiesCol);
-  const cityList = citySnapshot.docs.map(doc => doc.data());
-  return cityList;
-}
-*/
+  if (user && user.password === password) {
+    // User authentication successful
+    return true;
+  } else {
+    // User authentication failed
+    return false;
+  }
 };
