de5f127a418db307ba27ed4e6405eb45457fb96b
Memehub <133832589+Memehub-Network@users.noreply.github.com>
2023-06-09 01:44:22 +0300

    Update systemAuth.js

--- functions/systemAuth.js
+++ functions/systemAuth.js
@@ -1,38 +1,16 @@
-exports = async function(arg){
-  // This default function will get a value and find a document in MongoDB
-  // To see plenty more examples of what you can do with functions see: 
-  // https://www.mongodb.com/docs/atlas/app-services/functions/
+exports = async function(req, res) {
+  const email = req.body.email;
+  const password = req.body.password;
 
-  // Find the name of the MongoDB service you want to use (see "Linked Data Sources" tab)
-  var serviceName = "mongodb-atlas";
-
-  // Update these to reflect your db/collection
-  var dbName = "db_name";
-  var collName = "coll_name";
-
-  // Get a collection from the context
-  var collection = context.services.get(serviceName).db(dbName).collection(collName);
-
-  var findResult;
   try {
-    // Get a value from the context (see "Values" tab)
-    // Update this to reflect your value's name.
-    var valueName = "value_name";
-    var value = context.values.get(valueName);
-
-    // Execute a FindOne in MongoDB 
-    findResult = await collection.findOne(
-      { owner_id: context.user.id, "fieldName": value, "argField": arg},
-    );
-
-  } catch(err) {
-    console.log("Error occurred while executing findOne:", err.message);
-
-    return { error: err.message };
+    const result = await context.functions.execute("authenticateUser", email, password);
+    if (result) {
+      res.status(200).json({ message: "Authentication successful" });
+    } else {
+      res.status(401).json({ message: "Authentication failed" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
   }
-
-  // To call other named functions:
-  // var result = context.functions.execute("function_name", arg1, arg2);
-
-  return { result: findResult };
 };
