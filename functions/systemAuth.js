b0b959dc1df5e7bdced5f86b8748b4f8e4ddfa6f
Memehub <133832589+Memehub-Network@users.noreply.github.com>
2023-06-09 02:34:33 +0300

    Update systemAuth.js

--- functions/systemAuth.js
+++ functions/systemAuth.js
@@ -5,12 +5,15 @@ exports = async function(req, res) {
   try {
     const result = await context.functions.execute("Function_0", email, password);
     if (result) {
-      res.status(200).json({ message: "Authentication successful" });
+      //res.status(200).json({ message: "Authentication successful" });
+return "Authentication successful";
     } else {
-      res.status(401).json({ message: "Authentication failed" });
+return "Authentication failed";
+      //res.status(401).json({ message: "Authentication failed" });
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Internal server error" });
+    //res.status(500).json({ message: "Internal server error" });
+return "Internal server error";
   }
 };
