e30475af4406480768a765ae190fe844875b5573
Memehub <133832589+Memehub-Network@users.noreply.github.com>
2023-06-09 02:56:43 +0300

    Update Function_0.js

--- functions/Function_0.js
+++ functions/Function_0.js
@@ -1,22 +1,23 @@
-exports = async function(email, password) {
+exports = async function(username, email, password) {
 
-  var cluster = "mongodb-atlas"; 
+   var cluster = "mongodb-atlas"; 
    var dBase = "authentication"; 
    var coll = "users"; 
   
-    const usersCollection = context.services.get(cluster).db(dBase).collection(coll); 
+   const usersCollection = context.services.get(cluster).db(dBase).collection(coll); 
  
 
-  //const usersCollection = context.services.get("mongodb-atlas").db("your-database").collection("users");
+   //const usersCollection = context.services.get("mongodb-atlas").db("your-database").collection("users");
 
-  // Find the user document based on the email
-  const user = await usersCollection.findOne({ email: email });
+   // Find the user document based on the email
+   const user = await usersCollection.findOne({ email: email });
 
-  if (user && user.password === password) {
-    // User authentication successful
-    return true;
-  } else {
-    // User authentication failed
-    return false;
-  }
+
+   if (user && user.password === password) {
+     // User authentication successful
+     return true;
+   } else {
+     // User authentication failed
+     return false;
+   }
 };
