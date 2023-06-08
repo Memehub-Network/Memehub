84dfed771a66e513a38daef25049c9cf07dafe32
Memehub <133832589+Memehub-Network@users.noreply.github.com>
2023-06-09 02:30:59 +0300

    Update systemAuth.js

--- functions/systemAuth.js
+++ functions/systemAuth.js
@@ -1,32 +1,9 @@
 exports = async function(req, res) {
-
   const email = req.body.email;
   const password = req.body.password;
 
-  var cluster = "mongodb-atlas"; 
-   var dBase = "authentication"; 
-   var coll = "users";
-  
-var sure = "logged in";
-var unsure = "error logging in";
-
-    const collection = context.services.get(cluster).db(dBase).collection(coll); 
- 
-  // Find the user document based on the email
-  const user = await collection.findOne({ email: email });
-
-  if (user && user.password === password) {
-    // User authentication successful
-    return sure;
-  } else {
-    // User authentication failed
-    return unsure;
-  }
-};
-
-/*
   try {
-    const result = await context.functions.execute("authenticateUser", email, password);
+    const result = await context.functions.execute("Function_0", email, password);
     if (result) {
       res.status(200).json({ message: "Authentication successful" });
     } else {
@@ -35,4 +12,5 @@ var unsure = "error logging in";
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
-  }*/
+  }
+};
