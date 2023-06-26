255ce98139a9c692e377560af16f1ce7af271a3b
Memehub <133832589+Memehub-Network@users.noreply.github.com>
2023-06-26 19:19:01 +0300

    Update systemAuth.js

--- functions/systemAuth.js
+++ functions/systemAuth.js
@@ -2,7 +2,8 @@ exports = async function(req, res) {
 
   if(req.body === undefined) { 
        throw new Error(`Request body was not defined.`) 
-   }
+  }
+  
   const model = JSON.parse(req.body.text());
 
   const password = model.password;
@@ -11,42 +12,53 @@ exports = async function(req, res) {
   empty = "";
 
   if(model.username == empty){
-      return "username empty!";
-      
-  }else if(model.password == empty){
     res.setStatusCode(401);
-    return "password is null!";
-      
+    res.setBody(JSON.stringify({
+      "type": "login",
+      "statusCode": 401,
+      "statusMessage": "Username is empty!",
+      "resource_id": 1
+    }));
+  }else
+  if(model.password == empty){
+    res.setStatusCode(401);
+    res.setBody(JSON.stringify({
+      "type": "login",
+      "statusCode": 401,
+      "statusMessage": "Password is empty!",
+      "resource_id": 2
+    }));
   }else{
-      //username = model.username;
-      const pass = password;
-      try {
-         const result = await context.functions.execute("Function_0", username, pass);
-            if(result) {
-              res.setStatusCode(200); 
-              // tip: You can also use EJSON.stringify instead of JSON.stringify. 
-              res.setBody(JSON.stringify({
-    "type": "login",
-    "statusCode": 200,
-    "statusMessage": "Login Successful",
-    "resource_id": 3
-}
-));
-              
-            }else{
-              res.setStatusCode(401); 
-              // tip: You can also use EJSON.stringify instead of JSON.stringify. 
-              res.setBody(JSON.stringify({
-    "type": "login",
-    "statusCode": 401,
-    "statusMessage": "Login Failed",
-    "resource_id": 0
-}
-));
-            }
-      }catch(error){
-        res.setStatusCode(400); 
-        res.setBody(error.message);
+    //username = model.username;
+    const pass = password;
+    try {
+      const result = await context.functions.execute("Function_0", username, pass);
+      if(result) {
+        res.setStatusCode(201); 
+        res.setBody(JSON.stringify({
+          "type": "login",
+          "statusCode": 200,
+          "statusMessage": "Login Successful",
+          "resource_id": 3
+        }));
+        
+      }else{
+        res.setStatusCode(401); 
+        // tip: You can also use EJSON.stringify instead of JSON.stringify. 
+        res.setBody(JSON.stringify({
+          "type": "login",
+          "statusCode": 401,
+          "statusMessage": "Login Failed",
+          "resource_id": 4
+          
+        }));
+        
       }
-}
+      
+    }catch(error){
+      res.setStatusCode(400);
+      res.setBody(error.message);
+    }
+    
+  }
 };
