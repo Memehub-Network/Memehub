812ae606c5354d84eda3d57c833585bd2298b729
Memehub <133832589+Memehub-Network@users.noreply.github.com>
2023-09-09 21:20:35 +0300

    Update getpost.js

--- functions/getpost.js
+++ functions/getpost.js
@@ -1,41 +1,12 @@
 exports = async function(payload, response) {
-  /*
-  if(payload.body === undefined) {
-      throw new Error(`Request body was not defined.`)
-  }
-  */
  
   const pageNumber = payload.query.page;
- /*
- 
- exports = function(pageNumber) {
-  const pageSize = 5; // Number of items to return per page
-  const startIndex = (pageNumber - 1) * pageSize;
-  const endIndex = startIndex + pageSize;
-
-  const postsCollection = context.services.get("mongodb-atlas").db("yourDatabaseName").collection("yourCollectionName");
-
-  try {
-    // Query the database to get the items for the requested page
-    const posts = postsCollection.find({}).sort({ _id: -1 }).skip(startIndex).limit(pageSize).toArray();
-
-    return posts;
-  } catch (error) {
-    // Handle any errors that occur during the database query
-    return { error: error.message };
-  }
-};
- 
- 
- */
- 
- 
  
   var cluster = "mongodb-atlas";
   var dBase = "memehubclient";
   var coll = "posts";
   
-  const pageSize = 5; // Number of items to return per page
+  const pageSize = 5;
   const startIndex = (pageNumber - 1) * pageSize;
   const endIndex = startIndex + pageSize;
   
