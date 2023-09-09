10de140a804886bc6fcc38945d9fa2bdf386c0f6
Memehub <133832589+Memehub-Network@users.noreply.github.com>
2023-09-09 17:50:25 +0300

    Update searchResult.js

--- functions/searchResult.js
+++ functions/searchResult.js
@@ -1,83 +1,44 @@
 exports = async function(payload, response) {
-    var cluster = "mongodb-atlas";
-    var dBase = "memehubclient";
-    var coll = "search_Lab";
-
-var pageSize = 15;
-
+    const cluster = "mongodb-atlas";
+    const dBase = "memehubclient";
+    const coll = "search_Lab";
+    const pageSize = 15;
     const collection = context.services.get(cluster).db(dBase).collection(coll);
 
     const action = payload.query.search;
 
     if (action == null) {
-
-      const randomChoice = Math.random() < 0.5 ? "user" : "post";
-      
-        return [
-            {
-                type: randomChoice
-            }
-        ];
-}
+        const randomChoice = Math.random() < 0.5 ? "user" : "post";
+        return [{ type: randomChoice }];
+    }
 
     if (action === "drop") {
-        const result = await collection.drop();
-        return result;
+        return await collection.drop();
     } else if (action === "create") {
         const randomChoice = Math.random() < 0.5 ? "user" : "post";
-        if (randomChoice === "post"){
-
-        const document = {
-            type: randomChoice
-        };
-       const insertResult = await collection.insertOne(document);
-}else{
         const document = {
             type: randomChoice,
-username: randomChoice + "meme",
-name: randomChoice + "memename"
+            username: randomChoice + "meme",
+            name: randomChoice + "memename"
         };
-       const insertResult = await collection.insertOne(document);
-}
-        return result;
+        const insertResult = await collection.insertOne(document);
+        return insertResult;
     } else if (action === "insert") {
-        // Randomly choose between "user" and "post"
         const randomChoice = Math.random() < 0.5 ? "user" : "post";
-        if (randomChoice === "post"){
-
-        const document = {
-            type: randomChoice
-        };
-       const insertResult = await collection.insertOne(document);
-}else{
         const document = {
             type: randomChoice,
-username: randomChoice + "meme",
-name: randomChoice + "memename"
+            username: randomChoice + "meme",
+            name: randomChoice + "memename"
         };
-       const insertResult = await collection.insertOne(document);
-}
-
-        // Delete the inserted document (uncomment if needed)
-        // const deleteResult = await collection.deleteOne({ _id: insertResult.insertedId });
-
-        return [{ type: randomChoice }]; // Return the random choice as "type".
-    }else{
-
-
-       const results = await collection.find({}).sort({ _id: -1 }).limit(pageSize).toArray();
-
-
-      const randomChoice = Math.random() < 0.5 ? "user" : "post";
-      
-if(results != null){
-return results;
-}else{
-        return [
-            {
-                type: randomChoice
-            }
-        ];
-}
-}
+        const insertResult = await collection.insertOne(document);
+        return [{ type: randomChoice }];
+    } else {
+        const results = await collection.find({}).sort({ _id: -1 }).limit(pageSize).toArray();
+        if (results != null) {
+            return results;
+        } else {
+            const randomChoice = Math.random() < 0.5 ? "user" : "post";
+            return [{ type: randomChoice }];
+        }
+    }
 };
