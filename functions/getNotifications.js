9054cf214f9f298c7d4bb8491611f20a3d4ef4f1
Memehub <133832589+Memehub-Network@users.noreply.github.com>
2023-10-20 12:20:21 +0300

    Update getNotifications.js

--- functions/getNotifications.js
+++ functions/getNotifications.js
@@ -1,5 +1,5 @@
 exports = function(payload) {
    const mongodb = context.services.get("mongodb-atlas");
    const mycollection = mongodb.db("memehubclient").collection("notifications");
-   return mycollection.find({}).limit(15).toArray();
+   return mycollection.find({}).limit(12).toArray();
 };
